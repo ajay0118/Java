6cd921603042ee9e1d0e7c83ced73d3d26009b73
ajayvenkateshps <149811885+ajayvenkateshps@users.noreply.github.com>
2023-11-30 14:23:43 +0530

    Addition.java

--- Addition.java
+++ Addition.java
@@ -1,3 +1,4 @@
+//sum of two integers.
 import java.util.Scanner;
 import java.io.*;
 public class Addition
@@ -5,21 +6,31 @@ public class Addition
 
 	public static void main(String[] args)
 	{
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 	
-	int n1;
-	int n2;
-	int sum;
+		int n1;
+		int n2;
+		int sum;
 	
-	System.out.println("Enter first number: ");	
-	n1 = input.nextInt();
+		System.out.println("Enter first number: ");	
+		n1 = input.nextInt();
 	
-	System.out.println("Enter second number: ");
-	n2 = input.nextInt();
+		System.out.println("Enter second number: ");
+		n2 = input.nextInt();
 	
-	sum = n1 + n2;
+		sum = n1 + n2;
 	
-	System.out.printf("sum is %d%n",sum);
-
+		System.out.printf("sum is %d%n",sum);
 	}
 }
+
+
+//output :
+/*
+Enter first number: 
+2
+Enter second number: 
+3
+sum is 5
+*/
+

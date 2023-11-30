bbe61d7f219ecdac761603273d4c9974806ad9f8
ajayvenkateshps <149811885+ajayvenkateshps@users.noreply.github.com>
2023-11-30 13:50:15 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+public class Addition
+{
+
+	public static void main(String[] args)
+	{
+	Scanner input = new Scanner(System.in);
+	
+	int n1;
+	int n2;
+	int sum;
+	
+	System.out.println("Enter first number: ");	
+	n1 = input.nextInt();
+	
+	System.out.println("Enter second number: ");
+	n2 = input.nextInt();
+	
+	sum = n1 + n2;
+	
+	System.out.printf("sum is %d%n",sum);
+
+	}
+}
